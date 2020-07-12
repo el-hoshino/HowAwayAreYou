429e12ef1cb745e228ca44428a07bd8259b7aa1b
史 翔新 <x_shi@yumemi.co.jp>
2020-07-12 19:18:32 +0900

    Initial Xcode project

---
+++ HowAwayAreYouTests/HowAwayAreYouTests.swift
@@ -0,0 +1,34 @@
+//
+//  HowAwayAreYouTests.swift
+//  HowAwayAreYouTests
+//
+//  Created by 史 翔新 on 2020/07/12.
+//  Copyright © 2020 Crazism. All rights reserved.
+//
+
+import XCTest
+@testable import HowAwayAreYou
+
+class HowAwayAreYouTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

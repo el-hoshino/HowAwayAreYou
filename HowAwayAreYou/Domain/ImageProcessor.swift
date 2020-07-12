ff9c3a25561d2f3030b6660bee1532d2d701a655
史 翔新 <x_shi@yumemi.co.jp>
2020-07-12 19:35:35 +0900

    Add basic source files without any implementations

---
+++ HowAwayAreYou/Domain/ImageProcessor.swift
@@ -0,0 +1,9 @@
+//
+//  ImageProcessor.swift
+//  HowAwayAreYou
+//
+//  Created by 史 翔新 on 2020/07/12.
+//  Copyright © 2020 Crazism. All rights reserved.
+//
+
+import Foundation
---
+++ HowAwayAreYou/UI/CameraFinderView.swift
@@ -0,0 +1,9 @@
+//
+//  CameraFinderView.swift
+//  HowAwayAreYou
+//
+//  Created by 史 翔新 on 2020/07/12.
+//  Copyright © 2020 Crazism. All rights reserved.
+//
+
+import Foundation

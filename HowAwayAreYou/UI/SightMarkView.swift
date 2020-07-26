58ee64e2ed46bbc7687da1d2845beceec6815025
史 翔新 <x_shi@yumemi.co.jp>
2020-07-26 21:39:30 +0900

    Update SightMarkView to draw arcs directly with Circle instead of Path, because it doesn't require GeometryReader

--- HowAwayAreYou/UI/SightMarkView.swift
+++ HowAwayAreYou/UI/SightMarkView.swift
@@ -38,94 +38,65 @@ struct SightMarkView: View {
     
     let status: Status
     
-    @State private var topLeftAngle: Double = 0
-    @State private var topRightAngle: Double = 0
-    @State private var bottomRightAngle: Double = 0
-    @State private var bottomLeftAngle: Double = 0
-    
+    @State private var angles: [Double] = [0, 0, 0, 0]
+        
     var body: some View {
-        GeometryReader { proxy in
-            Path.topLeftArc(proxy: proxy, rotation: .radians(self.topLeftAngle), color: self.status.color)
-                .setupAnimationOnAppear(flag: self.status.spins, onFlagTrue: { self.topLeftAngle += .pi * 2 }, onFlagFalse: { self.topLeftAngle = 0 })
-            Path.topRightArc(proxy: proxy, rotation: .radians(self.topRightAngle), color: self.status.color)
-                .setupAnimationOnAppear(flag: self.status.spins, onFlagTrue: { self.topRightAngle -= .pi * 2 }, onFlagFalse: { self.topRightAngle = 0 })
-            Path.bottomRightArc(proxy: proxy, rotation: .radians(self.bottomRightAngle), color: self.status.color)
-                .setupAnimationOnAppear(flag: self.status.spins, onFlagTrue: { self.bottomRightAngle += .pi * 2 }, onFlagFalse: { self.bottomRightAngle = 0 })
-            Path.bottomLeftArc(proxy: proxy, rotation: .radians(self.bottomLeftAngle), color: self.status.color)
-                .setupAnimationOnAppear(flag: self.status.spins, onFlagTrue: { self.bottomLeftAngle -= .pi * 2 }, onFlagFalse: { self.bottomLeftAngle = 0 })
+        ZStack {
+            Circle()
+                .inset(by: 5)
+                .trim(from: 0.02, to: 0.23)
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .butt))
+                .fill(status.color)
+                .rotationEffect(.radians(angles[0]))
+            Circle()
+                .inset(by: 10)
+                .trim(from: 0.27, to: 0.48)
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .butt))
+                .fill(status.color)
+                .rotationEffect(.radians(-angles[1]))
+            Circle()
+                .inset(by: 12)
+                .trim(from: 0.52, to: 0.73)
+                .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .butt))
+                .fill(status.color)
+                .rotationEffect(.radians(angles[2]))
+            ZStack {
+                Circle()
+                    .trim(from: 0.77, to: 0.98)
+                    .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .butt))
+                    .fill(status.color)
+                
+                Circle()
+                    .inset(by: 15)
+                    .trim(from: 0.77, to: 0.98)
+                    .stroke(style: StrokeStyle(lineWidth: 12, lineCap: .butt))
+                    .fill(status.color)
+            }
+                .rotationEffect(.radians(-angles[3]))
+        }
+        .onAppear {
+            self.toggleSpinAnimation(spins: self.status.spins)
         }
     }
     
-}
-
-private extension GeometryProxy {
-    
-    var center: CGPoint {
-        .init(x: size.width / 2, y: size.height / 2)
-    }
-    
-    var fitRadius: CGFloat {
-        min(size.width, size.height) / 2
-    }
-    
-}
-
-private extension Animation {
-    
-    static func repeatAnimation(duration: TimeInterval) -> Animation {
-        linear(duration: duration).repeatForever(autoreverses: false)
-    }
-    
-}
-
-private extension View {
-    
-    func setupAnimationOnAppear(flag: Bool, onFlagTrue: @escaping () -> Void, onFlagFalse: @escaping () -> Void) -> some View {
-        onAppear {
-            if flag == true {
-                withAnimation(Animation.repeatAnimation(duration: .random(in: 2 ... 5)), onFlagTrue)
-            } else {
-                withAnimation(.easeOut(duration: 0.2), onFlagFalse)
+    func toggleSpinAnimation(spins: Bool) {
+        for i in self.angles.indices {
+            withAnimation(.strokeAnimation(spins: spins)) {
+                self.angles[i] = spins ? .pi * 2 : 0
             }
         }
     }
     
 }
 
-private extension Path {
+private extension Animation {
     
-    private static let marginDegrees: Double = 5
-    private typealias ArcAngles = (startAngle: Angle, endAngle: Angle)
-    private static let topLeftArcAngles: ArcAngles = ((.degrees(180 + marginDegrees), .degrees(270 - marginDegrees)))
-    private static let topRightArcAngles: ArcAngles = ((.degrees(270 + marginDegrees), .degrees(0 - marginDegrees)))
-    private static let bottomRightArcAngles: ArcAngles = ((.degrees(0 + marginDegrees), .degrees(90 - marginDegrees)))
-    private static let bottomLeftArcAngles: ArcAngles = ((.degrees(90 + marginDegrees), .degrees(180 - marginDegrees)))
-    
-    private static func addArc(center: CGPoint, radius: CGFloat, angles: ArcAngles, rotation: Angle, color: Color, lineWidth: CGFloat) -> some View {
-        Path { path in
-            path.addArc(center: center, radius: radius, startAngle: angles.startAngle, endAngle: angles.endAngle, clockwise: false)
-        }
-        .rotation(rotation)
-        .stroke(color, lineWidth: lineWidth)
+    private static func repeatAnimation(duration: TimeInterval) -> Animation {
+        linear(duration: duration).repeatForever(autoreverses: false)
     }
     
-    static func topLeftArc(proxy: GeometryProxy, rotation: Angle, color: Color) -> some View {
-        Path.addArc(center: proxy.center, radius: proxy.fitRadius - 22, angles: topLeftArcAngles, rotation: rotation, color: color, lineWidth: 10)
-    }
-    
-    static func topRightArc(proxy: GeometryProxy, rotation: Angle, color: Color) -> some View {
-        ZStack {
-            Path.addArc(center: proxy.center, radius: proxy.fitRadius - 12, angles: topRightArcAngles, rotation: rotation, color: color, lineWidth: 4)
-            Path.addArc(center: proxy.center, radius: proxy.fitRadius - 30, angles: topRightArcAngles, rotation: rotation, color: color, lineWidth: 10)
-        }
-    }
-    
-    static func bottomRightArc(proxy: GeometryProxy, rotation: Angle, color: Color) -> some View {
-        Path.addArc(center: proxy.center, radius: proxy.fitRadius - 14, angles: bottomRightArcAngles, rotation: rotation, color: color, lineWidth: 10)
-    }
-    
-    static func bottomLeftArc(proxy: GeometryProxy, rotation: Angle, color: Color) -> some View {
-        Path.addArc(center: proxy.center, radius: proxy.fitRadius - 10, angles: bottomLeftArcAngles, rotation: rotation, color: color, lineWidth: 10)
+    static func strokeAnimation(spins: Bool) -> Animation {
+        spins ? repeatAnimation(duration: .random(in: 2 ... 5)) : .easeOut(duration: 0.2)
     }
     
 }

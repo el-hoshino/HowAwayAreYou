5de0072c52502415795587f58c1c520b043bed62
史 翔新 <x_shi@yumemi.co.jp>
2020-07-26 21:33:05 +0900

    Fix ImageData with more low level properties as optimization

--- HowAwayAreYou/Data/ImageData.swift
+++ HowAwayAreYou/Data/ImageData.swift
@@ -7,17 +7,71 @@
 //
 
 import UIKit
+import SwiftUI
 
 struct ImageData {
     
-    var image: UIImage
-    
-    init(uiImage: UIImage) {
-        self.image = uiImage
+    enum Orientation {
+        case up
+        case right
     }
     
-    init(cgImage: CGImage, scale: CGFloat, orientation: UIImage.Orientation) {
-        self.image = UIImage(cgImage: cgImage, scale: scale, orientation: orientation)
+    let cgImage: CGImage
+    let scale: CGFloat
+    let orientation: Orientation
+    
+    var image: UIImage {
+        UIImage(cgImage: cgImage, scale: scale, orientation: orientation.uiOrientation)
+    }
+    
+    init(uiImage: UIImage) {
+        self.cgImage = uiImage.cgImage!
+        self.scale = uiImage.scale
+        self.orientation = Orientation(uiOrientation: uiImage.imageOrientation)
+    }
+    
+    init(cgImage: CGImage, scale: CGFloat, orientation: Orientation) {
+        self.cgImage = cgImage
+        self.scale = scale
+        self.orientation = orientation
+    }
+    
+}
+
+extension ImageData.Orientation {
+    
+    init(uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+        case .up:
+            self = .up
+            
+        case .right:
+            self = .right
+            
+        default:
+            assertionFailure()
+            self = .up
+        }
+    }
+    
+    var uiOrientation: UIImage.Orientation {
+        switch self {
+        case .up:
+            return .up
+            
+        case .right:
+            return .right
+        }
+    }
+    
+    var swiftOrientation: Image.Orientation {
+        switch self {
+        case .up:
+            return .up
+            
+        case .right:
+            return .right
+        }
     }
     
 }
